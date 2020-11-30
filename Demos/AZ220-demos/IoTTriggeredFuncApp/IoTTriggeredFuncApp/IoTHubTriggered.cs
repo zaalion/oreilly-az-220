b195f1aa93d5e2e91965e776d0a93377b4b474d8
Reza Salehi <reza.salehi@newsignature.com>
2020-11-29 23:19:01 -0500

    Function App demo

---
+++ Demos/AZ220-demos/IoTTriggeredFuncApp/IoTTriggeredFuncApp/IoTHubTriggered.cs
@@ -0,0 +1,26 @@
+using IoTHubTrigger = Microsoft.Azure.WebJobs.EventHubTriggerAttribute;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.EventHubs;
+using System.Text;
+using System.Net.Http;
+using Microsoft.Extensions.Logging;
+
+namespace IoTTriggeredFuncApp
+{
+    public static class IoTHubTriggered
+    {
+        private static HttpClient client = new HttpClient();
+
+        /// <summary>
+        /// Use Event Hub-compatible endpoint in your connection string
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="log"></param>
+        [FunctionName("IoTHubTriggered")]
+        public static void Run([IoTHubTrigger("messages/events", Connection = "IotHubConnection")]EventData message, ILogger log)
+        {
+            log.LogInformation($"C# IoT Hub trigger function processed a message: {Encoding.UTF8.GetString(message.Body.Array)}");
+        }
+    }
+}
